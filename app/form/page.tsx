a441114cfac9a2ded912f37ca79efb2362ede63f
Suthiphong Thaisuriya <suthiphong.t@arise.tech>
2024-09-03 20:43:13 +0700

    version 14

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Stack, Button, Typography, Input } from "@mui/joy";
+import { Stack, Button, Typography, Input, Box } from "@mui/joy";
 import { useForm } from "react-hook-form";
 import { z  } from 'zod';
 import { DevTool } from "@hookform/devtools";
@@ -7,17 +7,21 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 export type FormValues = {
   firstname: string;
+  laserCode: string;
 }
 
 const schema = z.object({
   firstname: z.string()
-    .min(1, "fristname is required")
+    .min(1, "fristname is required"),
+  laserCode: z.string()
+    .min(1, "laser code is required"),
 });
 
 export default function Page() {
   const { register, watch, control, formState, getValues} = useForm<FormValues>({
     defaultValues: {
       firstname: "",
+      laserCode: "",
     },
     resolver: zodResolver(schema),
     mode: "onBlur",
@@ -25,7 +29,7 @@ export default function Page() {
 
   const { errors, isDirty, isValid } = formState;
 
-  console.log('version 13')
+  console.log('version 14')
 
   return (
     <Stack
@@ -38,36 +42,68 @@ export default function Page() {
     >
       <form onSubmit={(e) => e.preventDefault()}>
         <Stack spacing={2}>
-          <Typography level="title-lg">LaserCode: {watch('firstname')}</Typography> 
-          <Input 
-            placeholder="Laser Code"
-            size="lg"
-            { ...register("firstname") }
-            type="text"
-            error={!!errors.firstname}
-            slotProps={{
-              input: {
-                onInput: (e: any) => {
-                  console.log('onInput', e.currentTarget.value)
-                  console.log('data', e.data);
-                  if (e.currentTarget.value.length > 12) {
-                    //e.currentTarget.value = e.currentTarget.value.slice(0, 12);
-                    //e.target.selectionStart = e.target.selectionEnd = e.target.value.length;
-                    // console.log('form value', getValues("firstname"))
-                    e.target.value = getValues("firstname");
-                  }
+          <Box>
+            <Typography level="title-lg">firstname: {watch('firstname')}</Typography> 
+            <Input 
+              placeholder="firstname"
+              size="lg"
+              { ...register("firstname") }
+              type="text"
+              error={!!errors.firstname}
+              slotProps={{
+                input: {
+                  onInput: (e: any) => {
+                    if (e.currentTarget.value.length > 12) {
+                      e.target.value = getValues("firstname");
+                      return;
+                    }
+                    const value = e.currentTarget.value;
+                    const newValue = value.replace(/[^ก-๙0-9(). -]/g, '');
+                    e.currentTarget.value = newValue;
+                  },
+                  maxLength: 12
+                }
+              }}
+            />
+            { errors.firstname && (
+              <Typography sx={{color: 'red', margin: 0, padding: 0}}>{errors.firstname.message}</Typography>
+            )}
+          </Box>
+          <Box>
+            <Typography level="title-lg">laserCode: {watch('laserCode')}</Typography> 
+            <Input 
+              placeholder="laserCode"
+              size="lg"
+              { ...register("laserCode") }
+              type="text"
+              error={!!errors.laserCode}
+              slotProps={{
+                input: {
+                  onInput: (e: any) => {
+                    const oldValue = getValues("laserCode");
+                    const value = e.currentTarget.value;
+                    const newValue = value.replace(/[^A-Z0-9]/g, '');
+                    if (newValue.length > 0 && newValue.length < 2 && !/[A-Za-z]/.test(newValue)) {
+                      e.target.value = oldValue;
+                      return;
+                    } else if (newValue.length > 1 && newValue.length < 13 && !/[0-9]/.test(newValue)) {
+                      e.target.value = oldValue;
+                      return;
+                    } else if (newValue.length > 12) {
+                      e.target.value = oldValue;
+                      return;
+                    }
 
-                  const value = e.currentTarget.value;
-                  const newValue = value.replace(/[^ก-๙0-9(). -]/g, '');
-                  e.currentTarget.value = newValue;
-                },
-                // maxLength: 12
-              }
-            }}
-          />
-          { errors.firstname && (
-            <Typography sx={{color: 'red', margin: 0, padding: 0}}>{errors.firstname.message}</Typography>
-          )}
+                    e.currentTarget.value = newValue;
+                  },
+                  maxLength: 12
+                }
+              }}
+            />
+            { errors.firstname && (
+              <Typography sx={{color: 'red', margin: 0, padding: 0}}>{errors.firstname.message}</Typography>
+            )}
+          </Box>
           <DevTool 
             control={control} 
           />
