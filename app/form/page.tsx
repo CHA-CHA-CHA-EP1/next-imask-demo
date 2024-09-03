b0b3df3f338f2057f412b63b54d15324d6e97952
Suthiphong Thaisuriya <suthiphong.t@arise.tech>
2024-09-03 20:14:47 +0700

    version 9

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
 
   const { errors, isDirty, isValid } = formState;
 
-  console.log('version 8')
+  console.log('version 9')
 
   return (
     <Stack
@@ -43,6 +43,7 @@ export default function Page() {
             placeholder="Laser Code"
             size="lg"
             { ...register("firstname") }
+            type="text"
             error={!!errors.firstname}
             slotProps={{
               input: {
@@ -50,7 +51,6 @@ export default function Page() {
                   console.log('onInput', e.currentTarget.value)
                   if (e.currentTarget.value.length > 12) {
                     //e.currentTarget.value = e.currentTarget.value.slice(0, 12);
-                    e.target.selectionStart = e.target.selectionEnd = e.target.value.length;
                     e.preventDefault();
                     e.stopPropagation();
                     e.stopImmediatePropagation();
@@ -58,6 +58,7 @@ export default function Page() {
                     e.nativeEvent.preventDefault();
                     e.nativeEvent.stopPropagation();
                     e.nativeEvent.stopImmediatePropagation();
+                    e.target.selectionStart = e.target.selectionEnd = e.target.value.length;
                   }
                   const value = e.currentTarget.value;
                   const newValue = value.replace(/[^ก-๙0-9(). -]/g, '');
